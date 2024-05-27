bea1ffc728e2b1a6cef66ac61eb57cf4f44a4175
Tetka95 <martina.bobek95@gmail.com>
2024-05-27 23:05:13 +0200

    article 2 js

---
+++ homepage_script.js
@@ -0,0 +1,9 @@
+function randomNum1 () {
+    let input1 = document.getElementById("input1");
+    input1.value = Math.floor(Math.random()*1000) + 1;
+}
+
+function randomNum2 () {
+    let input2 = document.getElementById("input2");
+    input2.value = Math.floor(Math.random()*1000) + 1;
+}
