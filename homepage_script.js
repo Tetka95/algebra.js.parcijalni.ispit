e8accb79b0390f3e710015075d37f7c2c6924ecc
Tetka95 <martina.bobek95@gmail.com>
2024-05-28 15:06:18 +0200

    popravljen random number generator i button veciBroj

--- homepage_script.js
+++ homepage_script.js
@@ -1,9 +1,20 @@
 function randomNum1 () {
-    let input1 = document.getElementById("input1");
+    let input1 = document.getElementById("prvi");
     input1.value = Math.floor(Math.random()*1000) + 1;
 }
 
 function randomNum2 () {
-    let input2 = document.getElementById("input2");
+    let input2 = document.getElementById("drugi");
     input2.value = Math.floor(Math.random()*1000) + 1;
+}
+
+function veciBroj () {
+    let input1 = document.getElementById("prvi");
+    let input2 = document.getElementById("drugi");
+
+    if (input1.value > input2.value) {
+        return alert(input1.value + " je veći od " + input2.value);
+    } else {
+        return alert(input2.value + " je veći od " + input1.value);
+    }
 }
